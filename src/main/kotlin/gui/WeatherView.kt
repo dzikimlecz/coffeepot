67bd9ceac017d0146693f8d3a202757607e5650b
dzikimlecz <j.pul312@gmail.com>
2023-07-03 17:09:14 +0200

    added image displaying the weather

--- src/main/kotlin/gui/WeatherView.kt
+++ src/main/kotlin/gui/WeatherView.kt
@@ -1,8 +1,13 @@
 package me.dzikimlecz.coffeepot.gui
 
+import me.dzikimlecz.coffeepot.weatherImageProperty
 import tornadofx.*
 class WeatherView : View() {
     override val root = stackpane {
+        scrollpane {
+            imageview(weatherImageProperty) {
 
+            }
+        }
     }
 }
