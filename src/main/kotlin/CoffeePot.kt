05e77058251862b297418b6186bb5a01e4a68d12
dzikimlecz <j.pul312@gmail.com>
2023-07-03 15:59:17 +0200

    merged CoffeePot.kt to Main.kt

--- src/main/kotlin/CoffeePot.kt
+++
@@ -1,8 +0,0 @@
-package me.dzikimlecz.coffeepot
-
-import gui.MainView
-import tornadofx.App
-
-class CoffeePot : App(MainView::class) {
-
-}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,9 @@
 package me.dzikimlecz.coffeepot
 import tornadofx.launch
+import gui.MainView
+import tornadofx.App
 
 fun main(args: Array<String>) = launch<CoffeePot>(args)
+
+
+class CoffeePot : App(MainView::class)
