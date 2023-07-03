4d270d0d620491c2c8ba077061a6444a0b6c63f1
dzikimlecz <j.pul312@gmail.com>
2023-07-03 15:48:53 +0200

    added resources object

---
+++ src/main/kotlin/me/dzikimlecz/coffeepot/Resources.kt
@@ -0,0 +1,16 @@
+package me.dzikimlecz.coffeepot
+
+object Resources {
+    val location: String
+    val weatherImagePath: String
+
+
+    init {
+        // TODO Replace hardcoding with resources file
+        location = "Poznań"
+        weatherImagePath = "w.png"
+    }
+
+
+
+}
