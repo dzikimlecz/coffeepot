839f80d528a1584d0e5cd3d473796afe225cc795
dzikimlecz <j.pul312@gmail.com>
2023-07-07 11:52:13 +0200

    implemented app launching

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,21 @@
 package me.dzikimlecz.coffeepot
 
-fun main(args: Array<String>) {
+import me.dzikimlecz.coffeepot.gui.mainPane
+import java.awt.Dimension
+import javax.swing.JFrame
 
+fun main(args: Array<String>) {
+    startDaemon()
+    launchApp(args)
+
+}
+
+fun launchApp(args: Array<String>) {
+    val mainFrame = JFrame("coffeepot")
+    mainFrame.contentPane = mainPane
+    if ("--debug" in args) {
+        mainFrame.maximumSize = Dimension(480, 320)
+    }
+    mainFrame.isVisible = true
 }
 
