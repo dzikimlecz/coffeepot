d93d9984f2dddc0906348b20a3f57da7b9d039f6
dzikimlecz <j.pul312@gmail.com>
2023-07-09 17:37:50 +0200

    removed accidental code repetition

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -27,10 +27,6 @@ fun launchApp(args: Array<String>) {
         if ("--debug" in args) {
             maximumSize = Dimension(480, 320)
         }
-        contentPane = mainPane
-        if ("--debug" in args) {
-            maximumSize = Dimension(480, 320)
-        }
         addWindowListener(object : WindowAdapter() {
             override fun windowClosing(e: WindowEvent?) {
                 stopDaemon()
