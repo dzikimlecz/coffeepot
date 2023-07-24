c13bc8b9aa3d8e97d8659f77040f4d405e0128f3
dzikimlecz <j.pul312@gmail.com>
2023-07-24 22:22:48 +0200

    swapped names of properties and their mutable backends

--- src/main/kotlin/Daemon.kt
+++ src/main/kotlin/Daemon.kt
@@ -1,5 +1,10 @@
 package me.dzikimlecz.coffeepot
 
+
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.awt.image.BufferedImage
 import java.io.IOException
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
@@ -7,6 +12,7 @@ import java.util.concurrent.Executors
 import java.util.concurrent.ScheduledExecutorService
 import java.util.concurrent.TimeUnit.MINUTES
 import java.util.concurrent.TimeUnit.SECONDS
+import javax.imageio.ImageIO
 
 ///////////////////////////////////////////////////////////////////////////
 // DAEMON
@@ -44,10 +50,14 @@ fun startDaemon() {
         )
         scheduleAtFixedRate(
             {
-                getWeather(Resources.location)
-                fetchWeatherImage(
-                    Resources.location
-                )
+                try {
+                    getWeather(Resources.location)
+                    fetchWeatherImage(
+                        Resources.location
+                    )
+                } catch (e: IOException) {
+                    println(e.message)
+                }
             },
             0, 30,
             MINUTES
@@ -76,14 +86,51 @@ val timeProperty: Observable<String> = time
 // WEATHER
 ///////////////////////////////////////////////////////////////////////////
 
+private val http = OkHttpClient()
 
+val weatherProperty: Observable<String>
+    get() = weather
+private val weather = MutableObservable("Zimno jest")
+
+val weatherImageProperty: Observable<BufferedImage>
+    get() = weatherImage
+private val weatherImage =
+    MutableObservable<BufferedImage>(ImageIO.read(object {}.javaClass
+        .classLoader.getResource("w.png")))
 
 fun fetchWeatherImage(location: String) {
-
+    val request = Request.Builder()
+        .url("https://v2.wttr.in/$location.png")
+        .build()
+    val response: Response
+    try {
+        response = http.newCall(request).execute()
+    } catch (e: Exception) {
+        failFetchingWeather(location, e)
+    }
+    if (response.code != 200) {
+        failFetchingWeather(location)
+    }
+    val stream = response.body?.byteStream() ?: failFetchingWeather(location)
+    val img = ImageIO.read(stream)
+    weatherImage.value = img
 }
 
 fun getWeather(location: String) {
-
+    val request = Request.Builder()
+        .url("https://www.wttr.in/$location?format=3")
+        .build()
+    val response: Response
+    try {
+        response = http.newCall(request).execute()
+    } catch (e: Exception) {
+        failFetchingWeather(location, e)
+    }
+    if (response.code != 200) {
+        failFetchingWeather(location)
+    }
+    val weather = response.body?.string() ?: failFetchingWeather(location)
+    me.dzikimlecz.coffeepot.weather.value = weather
 }
 
 private fun failFetchingWeather(
