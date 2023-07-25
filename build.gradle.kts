c6933ee5fc30ec0bb94f0ac49e5115de505f104a
dzikimlecz <j.pul312@gmail.com>
2023-07-25 21:54:52 +0200

    added hibernate dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,9 +19,16 @@ dependencies {
     testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+
     val oneBusAwayVersion = "1.3.4"
     implementation("org.onebusaway:onebusaway-gtfs:$oneBusAwayVersion")
     implementation("org.onebusaway:onebusaway-gtfs-hibernate:$oneBusAwayVersion")
+
+    implementation("org.xerial:sqlite-jdbc:3.42.0.0")
+    implementation("com.github.gwenn:sqlite-dialect:0.1.4")
+    implementation("javax.transaction:jta:1.1")
+    implementation("org.hibernate:hibernate-annotations:3.3.1.GA")
+
     implementation(platform("com.squareup.okhttp3:okhttp-bom:4.11.0"))
     implementation("com.squareup.okhttp3:okhttp")
     implementation("com.squareup.okhttp3:logging-interceptor")
@@ -66,7 +73,7 @@ tasks.withType<Copy> {
 tasks.withType<JPackageTask> {
     type = ImageType.APP_IMAGE
     appName = "coffeepot"
-    appVersion = "0.1"
+    appVersion = version.toString()
     input = "build/libs/uber"
-    mainJar = "coffeepot-0.1-uber.jar"
+    mainJar = "coffeepot-$appVersion-uber.jar"
 }
