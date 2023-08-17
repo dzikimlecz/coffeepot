545fa737f5c6f291d90ba668ffe642fcbc16475b
dzikimlecz <j.pul312@gmail.com>
2023-08-17 06:05:48 +0200

    added properties for transit feed and app directory

--- src/main/kotlin/Resources.kt
+++ src/main/kotlin/Resources.kt
@@ -1,14 +1,20 @@
 package me.dzikimlecz.coffeepot
 
 object Resources {
+    val trackedStops: List<String>
     val location: String
     val weatherImagePath: String
+    val transitFeedUrl: String
+    val appDirectory: String
 
 
     init {
         // TODO Replace hardcoding with resources file
         location = "Poznań"
         weatherImagePath = "w.png"
+        transitFeedUrl = "https://ztm.poznan.pl/pl/dla-deweloperow/getGTFSFile"
+        appDirectory = System.getProperty("user.home") + "/.coffeepot"
+        trackedStops = listOf("Jasna Rola")
     }
 
 
