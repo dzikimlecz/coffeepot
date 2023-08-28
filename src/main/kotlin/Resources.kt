61aff312963c0a16f8f5296e88f4b4830b918a33
dzikimlecz <j.pul312@gmail.com>
2023-08-28 21:13:37 +0200

    replaced hard-coded properties with config file

--- src/main/kotlin/Resources.kt
+++ src/main/kotlin/Resources.kt
@@ -1,22 +1,77 @@
 package me.dzikimlecz.coffeepot
 
+import java.io.File
+import java.util.Properties
+
 object Resources {
-    val trackedStops: List<String>
     val location: String
-    val weatherImagePath: String
+    val locationCode: Pair<String, String>
     val transitFeedUrl: String
     val appDirectory: String
+    val trackedStopCodes: List<String>
+    val trackedStopNames: List<String>
 
+    // TODO: remove
+    val weatherImagePath: String = "w.png"
+    val trackedStops: List<String>
+        get() = trackedStopNames
 
     init {
-        // TODO Replace hardcoding with resources file
-        location = "Poznań"
-        weatherImagePath = "w.png"
-        transitFeedUrl = "https://ztm.poznan.pl/pl/dla-deweloperow/getGTFSFile"
-        appDirectory = System.getProperty("user.home") + "/.coffeepot"
-        trackedStops = listOf("Jasna Rola")
+
+        val configFile = File(
+            System.getProperty("user.home"),
+            ".config/coffeepot/config.properties"
+        )
+
+        val properties = readProperties(configFile)
+
+        location = properties["location"].toString()
+        transitFeedUrl = properties["transitFeedUrl"].toString()
+        appDirectory = properties["appDirectory"].toString()
+        trackedStopNames = properties["stopNames"]!!.toString()
+            .split(",")
+            .map(String::trim)
+        trackedStopCodes = properties["stopCodes"]!!.toString()
+            .split(",")
+            .map(String::trim)
+
+        locationCode = with(properties["locationCode"]!!.toString()) {
+            if(isEmpty()) {
+                "" to ""
+            } else {
+                val tokens = split(",")
+                check(tokens.size == 2) {
+                    "Malformed locationCode: $this. (Proper example: '1, 2')."
+                }
+                tokens[0] to tokens[1]
+            }
+        }
+
     }
 
-
-
 }
+
+private fun readProperties(configFile: File): Properties {
+    require(configFile.exists()) { "File ${configFile.path} does not exist." }
+    require(configFile.isFile) { "File ${configFile.path} is a directory." }
+    require(configFile.canRead()) { "File ${configFile.path} is unreadable." }
+
+    val defaults = Properties().apply {
+        this["appDirectory"] =
+            System.getProperty("user.home") + "/.coffeepot"
+        this["locationCode"] = ""
+        this["stopNames"] = ""
+        this["stopCodes"] = ""
+        this["transitFeedUrl"] = ""
+    }
+
+    val properties = Properties(defaults)
+        .apply { load(configFile.inputStream()) }
+
+    requireNotNull(properties["location"]) {
+        "Location of weather services must be set."
+    }
+
+    return properties
+}
+
