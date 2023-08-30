f176b22985664f7cd551385ae3dbde521f82d5c2
dzikimlecz <j.pul312@gmail.com>
2023-08-30 11:44:04 +0200

    added option to pass a config file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,8 @@ import me.dzikimlecz.coffeepot.gui.mainPane
 import java.awt.Dimension
 import java.awt.event.WindowAdapter
 import java.awt.event.WindowEvent
+import java.io.File
+import java.io.FileNotFoundException
 import javax.swing.JFrame
 import kotlin.system.exitProcess
 
@@ -25,6 +27,10 @@ fun init() {
 val debugOn: Boolean
     get() = debug ?: false
 
+val configFile
+    get() = config
+
+private var config: File? = null
 private var debug: Boolean? = null
 
 fun launchApp(args: Array<String>) {
@@ -32,6 +38,24 @@ fun launchApp(args: Array<String>) {
         throw IllegalStateException("Application has already been started")
     }
     debug = "--debug" in args
+    val configFileSet = "--config" in args || "-c" in args
+    if (configFileSet) {
+        var pathIx = args.indexOf("--config") + 1
+        if (pathIx == 0) {
+            pathIx = args.indexOf("-c") + 1
+        }
+        require(pathIx < args.size) {
+            "Empty config path!"
+        }
+        val configFile = File(args[pathIx])
+        if (!configFile.exists()) {
+            throw FileNotFoundException("File ${args[pathIx]} not found.")
+        }
+        require(configFile.isFile && configFile.canRead()) {
+            "Can't read from ${args[pathIx]}"
+        }
+        config = configFile
+    }
     val mainFrame = JFrame("coffeepot")
     with(mainFrame) {
         contentPane = mainPane
